7c3f83cd175d2ea59ceb98034211c7c9a05e92f0
NikRian <nik04rian@gmail.com>
2023-03-28 13:39:17 -0300

    Lista 1

--- Lista 1/src/questão_1.java
+++ Lista 1/src/questão_1.java
@@ -4,33 +4,31 @@ public class questão_1 {
     public static void main(String[] args) {
 
         System.out.print("\033[H\033[2J");  
-        System.out.flush();
-
-        int A, B;
+        System.out.flush();  
 
         try (Scanner ler = new Scanner(System.in)) {
 
             System.out.print("\nInsira o valor de A: ");
-            A = ler.nextInt();
+            int A = ler.nextInt();
 
             System.out.print("\nInsira o valor de B: ");
-            B = ler.nextInt();
-        }
+            int B = ler.nextInt();
 
-        if (A > 10) {
+            if (A > 10) {
 
-            System.out.println("\nA > 10");
+                System.out.println("\nA > 10");
 
-        }
+            }
 
-        if (A + B == 20) {
+            if (A + B == 20) {
 
-            System.out.println("\nA + B == 20");
+                System.out.println("\nA + B == 20");
 
-         } else {
+            } else {
 
-            System.out.println("\nnúmero não válido");
+                System.out.println("\nnúmero não válido");
 
+            }
         }        
     }
 }
